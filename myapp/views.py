17e6d10ac89612bf0749040283bd5698f1146209
danieldenton <danielmdenton@gmail.com>
2022-06-19 21:28:59 -0700

    word counter

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        'name': 'Sav',
+        'age': '37',
+        'nationallity': 'American',
+    }
+    return render(request, 'index.html', context)
+
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
