7f44d7481ec7522f94781f9ad2bff167ee54e7a6
danieldenton <danielmdenton@gmail.com>
2022-06-20 12:26:08 -0700

    intro models

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 
 def index(request):
-    context = {
-        'name': 'Sav',
-        'age': '37',
-        'nationallity': 'American',
-    }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = "Fast"
+    feature1.details = "Our service is very quick"
+    feature1.is_true = True
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = "Reliable"
+    feature2.details = "Our service is very reliable"
+    feature2.is_true = False
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = "Easy to Use"
+    feature3.details = "Our service is very easy"
+    feature3.is_true = False
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = "Affordable"
+    feature4.details = "Our service is very affordable"
+    feature4.is_true = True
+
+    features = [feature1, feature2, feature3, feature4]
+    return render(request, 'index.html', {'features': features})
 
 
 def counter(request):
